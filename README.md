f77afe91ffe69922807bbb8fc64226d83ed1cd2b
Maria Perlovskaya <marigreenzelen@gmail.com>
2019-03-21 13:09:52 +0100

    Задание 3. Качество кода.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # project-lvl1-s462
 <a href="https://codeclimate.com/github/green-zelen/project-lvl1-s462/maintainability"><img src="https://api.codeclimate.com/v1/badges/5847ec9729143dc8c94b/maintainability" /></a>
+<a href="https://travis-ci.org/green-zelen/project-lvl1-s462.svg?branch=master"></a>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
+
 const askUserName = () => {
-    const userName = readlineSync.question('May I have your name?');
-    console.log(`Hello, ${userName}!`)
-}
+  const userName = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${userName}!`);
+};
 export default askUserName;
