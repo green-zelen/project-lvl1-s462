2e630a2e9af7f9daabffba8351c1ae7f03bea79a
Maria Perlovskaya <marigreenzelen@gmail.com>
2019-04-01 21:51:04 +0200

    Правки алгоритма для простого числа.

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,16 +3,14 @@ import playBrainGame from '../index';
 
 const desc = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const minRnd = 4;
-const maxRnd = 4;
-
+const minRnd = 1;
+const maxRnd = 100;
 const isPrime = (num) => {
-  for (let i = 2; i < Math.floor(num / 2); i += 1) {
+  for (let i = 2; i < Math.floor(num / 2 + 1); i += 1) {
     if (num % i === 0) return false;
   }
   return num > 1;
 };
-
 const getQuestionAndAnswer = () => {
   const question = getRandInt(minRnd, maxRnd);
   const correctAnswer = isPrime(question) ? 'yes' : 'no';
